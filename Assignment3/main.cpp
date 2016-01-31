69716cc8fd177f4acf4bdda7267d97621782042b
Peter Pan <ppan@eng.utah.edu>
2016-01-31 02:49:01 -0700

    Added assignment 3 initial files.

---
+++ Assignment3/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Owner
+ *
+ * Created on January 31, 2016, 2:21 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
