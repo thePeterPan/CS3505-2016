817016815833eba9bc257099fd061c9c2ea667b8
Peter Pan Desktop <peter.pan@netxoft.com>
2016-02-15 03:57:30 -0700

    Edits for partner folder to build in travis.

---
+++ Assignment4-Zhi/TrieUnitTests.cpp
@@ -0,0 +1,19 @@
+#include "Node.h"
+#include "Trie.h"
+#include "gtest/gtest.h"
+
+#include <iostream>
+#include <string>
+
+// TEST(test_case_name, test_name) {
+//  ... test body ...
+// }
+
+TEST(TrieUnitTests, Dummy) {
+	ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
