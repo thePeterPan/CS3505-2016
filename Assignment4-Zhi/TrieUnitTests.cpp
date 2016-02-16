adbdcf24b3bbab41b5940b4c94ade2c1d3d635d3
Zhi Wang <zhiw@lab1-2.eng.utah.edu>
2016-02-16 13:53:29 -0700

    first attampt

--- Assignment4-Zhi/TrieUnitTests.cpp
+++ Assignment4-Zhi/TrieUnitTests.cpp
@@ -1,7 +1,7 @@
 #include "Trie/Node.h"
 #include "Trie/Trie.h"
 #include "gtest/gtest.h"
-
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -10,8 +10,47 @@
 // }
 
 TEST(TrieUnitTests, Dummy) {
-	ASSERT_EQ(1, 1);
+    Trie trie;
+    trie.addWord("cnm");
+    
+	ASSERT_EQ(true, trie.isWord("cnm"));
 }
+TEST(TrieUnitTests, Dummy1) {
+    Trie trie;
+    trie.addWord("cnm");
+    
+	ASSERT_EQ(false, trie.isWord("c"));
+}
+TEST(TrieUnitTests, Dummy2) {
+    Trie trie;
+    ifstream input("dictionary.txt");
+    for(string inputStr; !input.eof();)
+	{
+		input >> inputStr;
+		trie.addWord(inputStr);
+	}
+	/* close the stream*/
+	input.close();
+    
+	ASSERT_EQ(true, trie.isWord("people"));
+}
+TEST(TrieUnitTests, Dummy3) {
+    Trie trie;
+    ifstream input("dictionary.txt");
+    for(string inputStr; !input.eof();)
+	{
+		input >> inputStr;
+		trie.addWord(inputStr);
+	}
+	/* close the stream*/
+	input.close();
+	vector<string> v = trie.allWordsWithPrefix("pe");
+    
+	ASSERT_EQ(16, v.size());
+}
+
+
+
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
