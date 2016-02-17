0f5c6640fd0a9c443fd1d91fb075678687dc8c1b
Peter Pan Desktop <peter.pan@netxoft.com>
2016-02-16 23:10:55 -0700

    Added some more tests, and a test.txt to go with them.

--- Assignment4/TrieUnitTests.cpp
+++ Assignment4/TrieUnitTests.cpp
@@ -11,16 +11,16 @@
 // variable. 
 int refCount = 0;
 
-// TEST(test_case_name, test_name) {
-//  ... test body ...
-// }
-
 // The fixture for testing class TrieUnitTests
 class TrieUnitTests : public ::testing::Test {
 protected:
     // You can remove any or all of the following functions if its body
     // is empty.
     Trie trie;
+    Trie trie2;
+    
+    std::ifstream input;
+    std::ifstream input2;
     
     TrieUnitTests() {
         // You can do set-up work for each test here.
@@ -33,13 +33,16 @@ protected:
     virtual void SetUp() {
         // Code here will be called immediately after the constructor (right
         // before each test).       
-        refCount = 0;        
+        refCount = 0;
+        input.open("dictionary.txt");
+        input2.open("test.txt");
     }
     
     virtual void TearDown() {
         // Code here will be called immediately after each test (right
         // before the destructor).
-        
+        input.close();
+        input2.close();
     }
     
     // Objects declared here can be used by all tests in the test case for
@@ -47,27 +50,112 @@ protected:
 };
 
 /**
- * Test for adding a single word.
+ * Test adding a single word.
  */
-TEST_F(TrieUnitTests, TestIsWord) {
-    
-    trie.addWord("cnm");
+TEST_F(TrieUnitTests, AddWord) {
+    trie.addWord("lorem");
 
-    ASSERT_TRUE(trie.isWord("cnm"));
-    
-    refCount = 3;
-    std::cout << refCount << std::endl;
+    ASSERT_TRUE(trie.isWord("lorem"));    
 }
 
 /**
- * Test for checking the word that is not in the Trie
+ * Make sure that the characters are converted to lowercase.
  */
-TEST_F(TrieUnitTests, TestIsWord2) {
+TEST_F(TrieUnitTests, DISABLED_AddWordUppercase) {
+    trie.addWord("LoRem");
     
+    ASSERT_TRUE(trie.isWord("lorem"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddWordWithNewline) {
+    trie.addWord("lorem\n");
+    
+    ASSERT_TRUE(trie.isWord("lorem"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddEmptyWord) {
+    trie.addWord("");
+    
+    ASSERT_FALSE(trie.isWord("lorem"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddWordSubwordIsNotWord) {
+    trie.addWord("loremipsum");
+    
+    ASSERT_FALSE(trie.isWord("remi"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddWordExtendedWord) {
+    trie.addWord("lorem");
+    
+    ASSERT_FALSE(trie.isWord("loremipsum"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddWordSmallWords) {
+    char charArray[10] = {'a','b','c','d','e','f','g','h','i','k'};
+    for (int i = 0; i < 10; i++) {
+        std::string s(1, charArray[i]);
+        trie.addWord(s);
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        std::string s(1, charArray[i]);
+        ASSERT_TRUE(trie.isWord(s));
+    }
+    
+    ASSERT_FALSE(trie.isWord("lorem"));
+}
+
+/**
+ * 
+ */
+TEST_F(TrieUnitTests, AddWordFromDictionaryFile) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    
+    ASSERT_TRUE(trie.isWord("people"));
+    ASSERT_TRUE(trie.isWord("internet"));
+    ASSERT_FALSE(trie.isWord("rtdeds"));
+    ASSERT_TRUE(trie.isWord("trust"));
+}
+
+/**
+ * isWord should return false when checking an empty string.
+ */
+TEST_F(TrieUnitTests, IsWordEmpty) {
+    ASSERT_FALSE(trie.isWord(""));
+}
+
+TEST_F(TrieUnitTests, IsWordNewline) {
+    trie.addWord("lorem");
+    
+    ASSERT_TRUE(trie.isWord("lorem\n"));
+}
+
+/**
+ * Test for checking the word that is not in the Trie.
+ */
+TEST_F(TrieUnitTests, IsWordIsNotAWord) {
     trie.addWord("cnm");
     
     ASSERT_FALSE(trie.isWord("c"));
-    std::cout << refCount << std::endl;
 }
 
 /**
@@ -75,57 +163,181 @@ TEST_F(TrieUnitTests, TestIsWord2) {
  * has 25 chars at all.
  */
 TEST_F(TrieUnitTests, Dummy1) {
+    
+    for (std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
     refCount = 0;
-    Trie trie;
-    std::ifstream input("dictionary.txt");
-    for(std::string inputStr; !input.eof();)
-	{
-		input >> inputStr;
-		trie.addWord(inputStr);
-	}
-	/* close the stream*/
-	input.close();
-	 refCount = 0;
-	 Trie trie2;
-	std::ifstream input2("test.txt");
-    for(std::string inputStr; !input2.eof();)
-	{
-		input2 >> inputStr;
-		trie2.addWord(inputStr);
-	}
-	/* close the stream*/
-	input2.close();
+    
+    for (std::string inputStr; !input2.eof();) {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }
 
-	ASSERT_EQ(25, refCount);
+    ASSERT_EQ(25, refCount);
 }
 
 /**
  * Test for adding a single word after adding amount of words.
  */
 TEST_F(TrieUnitTests, Dummy2) {
-    Trie trie;
-    std::ifstream input("dictionary.txt");
-    for(std::string inputStr; !input.eof();)
-	{
-		input >> inputStr;
-		trie.addWord(inputStr);
-	}
-	/* close the stream*/
-	input.close();
-	
-	 Trie trie2;
-	std::ifstream input2("test.txt");
-    for(std::string inputStr; !input2.eof();)
-	{
-		input2 >> inputStr;
-		trie2.addWord(inputStr);
-	}
-	/* close the stream*/
-	input2.close();
-	
-	 trie2.addWord("cnm");
+    for (std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+
+    for (std::string inputStr; !input2.eof();)
+    {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }
+
+    trie2.addWord("cnm");
     
-	ASSERT_TRUE(trie2.isWord("cnm"));
+    ASSERT_TRUE(trie2.isWord("cnm"));
+}
+
+/**
+ * Count the number of words that have the specified prefix. 
+ */
+TEST_F(TrieUnitTests, AllWordWithPrefixCount) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    
+    std::vector<std::string> v(trie.allWordsWithPrefix("pe"));
+
+    ASSERT_EQ(16, v.size());
+}
+
+/**
+ * Count the number of words that have the specified prefix. 
+ */
+TEST_F(TrieUnitTests, AllWordWithPrefixCountNewLine) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    
+    std::vector<std::string> v(trie.allWordsWithPrefix("pe\n"));
+
+    ASSERT_EQ(16, v.size());
+}
+
+/**
+ * Count the number of words that have the specified prefix.
+ */
+TEST_F(TrieUnitTests, AllWordWithPrefixCountZero) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    
+    std::vector<std::string> v = trie.allWordsWithPrefix("asdfghjkl");
+
+    ASSERT_EQ(0, v.size());
+}
+
+/**
+ * Count the number of words that have the specified prefix where the prefix
+ * is the entire word.
+ */
+TEST_F(TrieUnitTests, AllWordWithPrefixCountWhole) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    std::vector<std::string> v = trie.allWordsWithPrefix("peace");
+
+    ASSERT_EQ(1, v.size());
+}
+
+// test for making different tries for adding and checking word exists
+TEST_F(TrieUnitTests, Dummy9) {
+    for(std::string inputStr; !input.eof();) {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+	
+    for(std::string inputStr; !input2.eof();) {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }	
+    
+    std::vector<std::string> v(trie2.allWordsWithPrefix("map"));
+    ASSERT_EQ(0, v.size());
+
+    std::vector<std::string> v1(trie2.allWordsWithPrefix("map"));
+    ASSERT_EQ(0, v1.size());
+
+    ASSERT_FALSE(trie2.isWord("map"));
+
+    std::vector<std::string> v3(trie2.allWordsWithPrefix("map"));
+    ASSERT_EQ(0, v3.size());
+}
+
+//counting nodes from dictionary and test 2 files 
+TEST_F(TrieUnitTests, Dummy10) {
+    refCount = 0;
+    for(std::string inputStr; !input.eof();)
+    {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+	
+    for(std::string inputStr; !input2.eof();)
+    {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }
+    ASSERT_EQ(9214, refCount);
+}
+
+//test for counting deleting nodes
+TEST_F(TrieUnitTests, Dummy11) {
+refCount = 0;
+    for(std::string inputStr; !input.eof();)
+    {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    ASSERT_EQ(9189, refCount);
+
+    refCount = 0;
+	
+    for(std::string inputStr; !input2.eof();)
+    {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }
+    ASSERT_EQ(25, refCount);
+
+    trie2.~Trie();
+
+    ASSERT_EQ(0, refCount);
+}
+
+// test for copy trie2 it should not have word map
+TEST_F(TrieUnitTests, DISABLED_Dummy12) {
+    for(std::string inputStr; !input.eof();)
+    {
+        input >> inputStr;
+        trie.addWord(inputStr);
+    }
+    for(std::string inputStr; !input2.eof();)
+    {
+        input2 >> inputStr;
+        trie2.addWord(inputStr);
+    }
+
+    Trie trie3(trie);
+    trie3 = trie2;
+
+    std::vector<std::string> v = trie3.allWordsWithPrefix("map");
+
+    ASSERT_EQ(0, v.size());
 }
 
 int main(int argc, char **argv) {
