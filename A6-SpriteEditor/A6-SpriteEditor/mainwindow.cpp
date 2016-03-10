b7451e3cd1b326c00544adbb2454d5b8db580992
Peter Pan Windows 10 VM <peter.pan@netxoft.com>
2016-03-10 03:28:18 -0700

    Got the quit function working.
    Also added UI objects to header although I don't think that it's
    necessary. Adjusted UI elements.

--- A6-SpriteEditor/A6-SpriteEditor/mainwindow.cpp
+++ A6-SpriteEditor/A6-SpriteEditor/mainwindow.cpp
@@ -6,9 +6,35 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connectSignalsAndSlots();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/**
+ * Connects all respective signals and slots.
+ *
+ * @brief MainWindow::connectSignalsAndSlots
+ */
+void MainWindow::connectSignalsAndSlots()
+{
+    // Menubar:
+//    connect(ui->actionNew_File, SIGNAL(triggered(bool)), this, SLOT());
+//    connect(ui->actionOpen, SIGNAL(triggered(bool)), this, SLOT());
+//    connect(ui->actionSave, SIGNAL(triggered(bool)), this, SLOT());
+//    connect(ui->actionSave_As, SIGNAL(triggered(bool)), this, SLOT());
+//    connect(ui->actionExport_As, SIGNAL(triggered(bool)), this, SLOT());
+//    connect(ui->actionImport,  SIGNAL(triggered(bool)), this, SLOT());
+    connect(ui->actionQuit, SIGNAL(triggered(bool)), this, SLOT(close()));
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    // http://stackoverflow.com/questions/25454648/qmainwindow-close-signal-not-emitted
+    event->ignore();
+    event->accept();
 }
--- A6-SpriteEditor/A6-SpriteEditor/mainwindow.h
+++ A6-SpriteEditor/A6-SpriteEditor/mainwindow.h
@@ -3,6 +3,16 @@
 
 #include <QMainWindow>
 
+#include <QPushButton>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QLabel>
+#include <QGraphicsView>
+
+#include <QCloseEvent>
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +25,45 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+
 private:
     Ui::MainWindow *ui;
+
+    /// UI Elements
+    QGridLayout *leftSidebar_GLayout;
+	    QPushButton *colorPicker_pushButton;
+        // Line *line_3;
+	    QSlider *playbackSpeed_horizontalSlider;
+	    QLabel *playbackSpeed_label;
+	    QGraphicsView *preview_graphicsView;
+	    QPushButton *shapes_pushButton;
+	    QPushButton *tools_pushButton;
+	    // Spacer *verticalSpacer_2;
+    QVBoxLayout *imageView_VLayout;
+    	QHBoxLayout *mainEditor_HLayout;
+    		QPushButton *nextFrame_pushButton;
+    		QPushButton *play_pushButton;
+    		QPushButton *prevFrame_pushButton;
+    	QGraphicsView *graphicsView;
+    QVBoxLayout *rightSidebar_VLayout;
+    	QPushButton *placeholder1;
+    	QPushButton *placeholder2;
+    	QPushButton *placeholder3;
+    	QPushButton *placeholder4;
+    	// Spacer *verticalSpacer;
+    // Line line;
+    // Line line_2;
+
+    void connectSignalsAndSlots();
+
+    virtual void closeEvent(QCloseEvent *event);
+
+private slots:
+
+
+protected:
+
 };
 
 #endif // MAINWINDOW_H
