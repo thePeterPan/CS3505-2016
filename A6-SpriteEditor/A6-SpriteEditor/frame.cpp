0563d593f6ed80a4b4dc0b8854a980dfbee3ed39
Peter Pan CADE Lab <ppan@eng.utah.edu>
2016-03-23 17:13:18 -0600

    completed basic structure of frame class

--- A6-SpriteEditor/A6-SpriteEditor/frame.cpp
+++ A6-SpriteEditor/A6-SpriteEditor/frame.cpp
@@ -1,6 +1,87 @@
 #include "frame.h"
 
-frame::frame(QObject *parent) : QObject(parent)
+/**
+ * Constructor
+ *
+ * @brief frame::frame
+ * @param parent
+ */
+frame::frame(QObject *parent) :
+    QObject(parent)
 {
 
+}
+
+/**
+ * Constructor
+ *
+ * @brief frame::frame
+ * @param parent
+ * @param width_
+ * @param height_
+ */
+frame::frame(QObject *parent, int width_, int height_) :
+    QObject(parent), width(width_), height(height_)
+{
+
+}
+
+/**
+ * Copy Constructor
+ *
+ * @brief frame::frame
+ * @param other
+ */
+frame::frame(const frame &other)
+{
+
+}
+
+/**
+ * @brief frame::setPixelColor
+ * @param x
+ * @param y
+ * @param color
+ */
+void frame::setPixelColor(int x, int y, QColor color)
+{
+
+}
+
+/**
+ * @brief frame::setWholeFrameColor
+ * @param color
+ */
+void frame::setWholeFrameColor(QColor color)
+{
+
+}
+
+/**
+ * @brief frame::getPixelColor
+ * @param x
+ * @param y
+ * @return
+ */
+QColor frame::getPixelColor(int x, int y)
+{
+    return QColor.black();
+}
+
+/**
+ * @brief frame::getFrameWidth
+ * @return
+ */
+int frame::getFrameWidth()
+{
+    return width;
+}
+
+/**
+ * @brief frame::getFrameHeight
+ * @return
+ */
+int frame::getFrameHeight()
+{
+    return height;
 }
--- A6-SpriteEditor/A6-SpriteEditor/frame.h
+++ A6-SpriteEditor/A6-SpriteEditor/frame.h
@@ -2,12 +2,27 @@
 #define FRAME_H
 
 #include <QObject>
+#include <QList>
+#include <QColor>
 
 class frame : public QObject
 {
     Q_OBJECT
 public:
     explicit frame(QObject *parent = 0);
+    explicit frame(QObject *parent = 0, int width_, int height_);
+    frame (const frame &other);
+
+    void setPixelColor(int x, int y, QColor color);
+    void setWholeFrameColor(QColor color);
+    QColor getPixelColor(int x, int y);
+    int getFrameWidth();
+    int getFrameHeight();
+
+private:
+    QList<QList<QColor>> frameMatrix;
+    int width;
+    int height;
 
 signals:
 
