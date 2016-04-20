429e69cac09facdc4850cd8907c9072041f011dc
keetonian <keeton.hodgson@gmail.com>
2016-04-20 09:35:22 -0600

    Merged in with the current gui project

--- A7-Parachutes/A7-Parachutes/gamewindow.cpp
+++ A7-Parachutes/A7-Parachutes/gamewindow.cpp
@@ -6,9 +6,22 @@ gameWindow::gameWindow(QWidget *parent) :
     ui(new Ui::gameWindow)
 {
     ui->setupUi(this);
+    sprite = Sprite();
+
+    QTimer::singleShot(100,this,SLOT(update()));
 }
 
 gameWindow::~gameWindow()
 {
     delete ui;
+}
+
+void gameWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    sprite.setX(sprite.getX() + 1);
+    sprite.setY(sprite.getY() + 1);
+    sprite.draw(&painter);
+
+    QTimer::singleShot(100,this,SLOT(update()));
 }
