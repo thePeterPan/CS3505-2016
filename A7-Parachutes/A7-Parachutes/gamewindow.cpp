2483d999796ff7f261c744088441be45c073cfea
KEVIN GRIGGS JR <griggsj@lab2-16.eng.utah.edu>
2016-04-20 09:37:00 -0600

    include box2d

--- A7-Parachutes/A7-Parachutes/gamewindow.cpp
+++ A7-Parachutes/A7-Parachutes/gamewindow.cpp
@@ -1,11 +1,13 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
+#include "Box2D/Box2D.h"
 
 gameWindow::gameWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::gameWindow)
 {
     ui->setupUi(this);
+
 }
 
 gameWindow::~gameWindow()
