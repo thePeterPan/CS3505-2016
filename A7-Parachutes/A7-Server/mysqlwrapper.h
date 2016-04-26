8b7b35e16378ee0e269cc9cc8362425931fceac3
tbarlow12 <tanner.barlow12@gmail.com>
2016-04-26 14:28:11 -0600

    Basic framework for mysql wrapper

--- A7-Parachutes/A7-Server/mysqlwrapper.h
+++ A7-Parachutes/A7-Server/mysqlwrapper.h
@@ -3,13 +3,18 @@
 
 #include <QObject>
 #include <QDebug>
-
+#include <QList>
 //MySQL Connector libraries
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
+
+//Name=neverland-db
+//Server=us-cdbr-azure-west-c.cloudapp.net
+//Login=b485a4f4f7fcea
+//Password=fd0282b9
 
 class MySQLWrapper : public QObject
 {
@@ -22,6 +27,19 @@ public:
     void setPassword(QString _password);
     void setDatabaseName(QString _name);
 
+    QList<QString> getTeacherWordsByLevel(QString teacher, int level);
+    bool loginAvailable(QString login);
+    bool isTeacher(QString login);
+    void insertNewStudent(QString login, QString first, QString last, QString password, QString teacher);
+    void insertNewTeacher(QString login, QString first, QString last, QString password);
+    int getUserCurrentLevel(QString login);
+
+    void updateUserScore(QString login, int score);
+    void updateUserLevel(QString login, int level);
+
+    void addTeacherWordsByLevel(QList<QString> words, QString login, int level);
+
+
     bool open();
     bool close();
 
@@ -30,6 +48,7 @@ public:
 signals:
 
 public slots:
+
 
 private:
     QString hostPath;
