33fffd51d727e25427fb26e7d2e702c6ded181fe
Keeton Hodgson <keeton.hodgson@gmail.com>
2016-04-28 10:24:05 -0600

    Forcing changes.
    GitHub wasn't accepting these changes for some reason.

--- A7-Parachutes/A7-Parachutes/gameLogic.h
+++ A7-Parachutes/A7-Parachutes/gameLogic.h
@@ -1,51 +1,99 @@
 #ifndef GAMELOGIC_H
 #define GAMELOGIC_H
-#include <QQueue>
+
 #include <QObject>
-#include "Box2D/Box2D.h"
-#include <QString>
+#include <QQueue>
+#include <QTimer>
 #include <QList>
+#include <QVector>
+#include <QDebug>
+
+#include "Box2D/Box2D.h"
 #include "temporarysprite.h"
 
-
-class gameLogic : public QObject
+class GameLogic : public QObject
 {
     Q_OBJECT
 
 public:
-    explicit gameLogic(QObject *parent = 0, float scale = 100.0f);
-    float getXPos();
-    float getYPos();
+    explicit GameLogic(QObject *parent = 0, int windowWidth = 800, int windowHeight = 635, float scale = 100.0f);
+    ~GameLogic();
+
+    // Getter functions
     int getCurrentLevel();
-    b2World* World;
+
+    // Other methods
+    void getWordsFromDatabase(int level); // Does this need to be public?
+
+    //
     void paintWorld(QPainter * painter);
-    b2World* getWorld();
-    void testSignals();
-    void getWordsFromDatabase(int level);
 
 private:
-    void setUpBox2D();
-    void addWordToWorld();
-    b2Vec2 position;
-    void CreateGround(float x, float y, float width, float height);
-    void CreateBox(QString letter, float x, float y, float width, float height, float friction = .8, float restitution = .6, float density = 1.3);
 
+    // Box2D //
+    // Parameters
+    b2World* World;
     float SCALE;
+    int windowWidth, windowHeight, windowHeight2;
+    int xScale, yScale;
+    // Functions
+    void setUpBox2D();
+    void CreateGround(float x, float y, float width, float height);
+    void createRoughGround();
 
-    QString currentWord;
-    int currentWordIndex;
-    QQueue<QString> words;
+    // Sprites //
+    // Parameters
     QList<TemporarySprite> sprites;
+    // Functions
+    void addWordToWorld();
+    b2Body * CreateBox(QString letter, float x, float y, float width, float height, float friction = .8, float restitution = .6, float density = 1.3);
+
+    //void gameOver();
+
+    // Words //
+    // Parameters
+    QString currentWord;
+    QString previousWord;
+    int currentWordIndex;
+    QQueue<QString> words; // Used with test words
+    QStringList wordsList; // Used with word input from file
+    bool fromFile;
+
+    // Game Play //
+    // Parameters
     int currentLevel;
+    int timerSeconds;
+    int timerFactor = 8;
+    QTimer* timer;
+    bool readyToPlay = false;
+    int score;
+    int missTypePenalty = 50;
+    int completeWordReward = 100; //This Score bonus is multiplied by seconds left on the timer
+    // Functions
+    void scoreChanged(int score);
+    void startNewTimer();
+
 
 signals:
-    void newWord(QString word);
-    void newLevel(int level);
-    void failed();
-    void victory();
+    void newWord(QString word); // GameWindow::receiveNewWord
+    void newLevel(int level);   // GameWindow::receiveNewLevel
+    void failed();              // GameWindow::receiveFail
+    void victory();             // GameWindow::receiveVictory
+    void updateActionTimer(QString message);    // GameWindow::actionTimerUpdated
+    void updateScore(QString score);            // GameWindow::scoreUpdated
+    void gameOver(int level, int score);        // GameWindow::on_gameOver_triggered
+    void levelCompleted(int level, int score);
 
 public slots:
-    void newLetterTyped(QChar letter);
+    void startGame();                   // GameWindow::readyToPlay
+    void pause();                       // GameWindow::pauseGame
+    void unPause();                     // GameWindow::unPauseGame
+    void newLetterTyped(QChar letter);  // GameWindow::letterTyped
+    void updateTimer();                 // Called by this->timer timeout()
+    void addWordsFromFile(QStringList); // GameWindow::addWordsFromFile
+    void changeSize(int newWidth, int newHeight); // GameWindow::newSize
+
+    //void restart();
 
 };
 
